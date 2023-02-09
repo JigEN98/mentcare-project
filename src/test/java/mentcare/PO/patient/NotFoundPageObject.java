a85c241a33eb274b10d0f19ad0f26997c8a48625
Diego <garofolo.dg@gmail.com>
2023-02-09 15:13:00 +0100

    fixed patient appointment list, now properly working

--- src/test/java/mentcare/PO/patient/NotFoundPageObject.java
+++ src/test/java/mentcare/PO/patient/NotFoundPageObject.java
@@ -1,4 +1,26 @@
 package mentcare.PO.patient;
 
-public class NotFoundPageObject {
+import mentcare.PO.LoginPageObject;
+import mentcare.PO.PageObject;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+
+public class NotFoundPageObject extends PageObject {
+    public NotFoundPageObject(WebDriver driver){super(driver);}
+
+    // ---------- Locators ----------
+    @FindBy(tagName = "b")
+    private WebElement Error;
+
+    @FindBy(xpath = "//a/button")
+    private WebElement ReturnLoginButton;
+
+    // ---------- Methods ----------
+    public String MessageError(){return Error.getText();}
+
+    public LoginPageObject ShowLogin(){
+        this.ReturnLoginButton.click();
+        return new LoginPageObject(driver);
+    }
 }
--- src/test/java/mentcare/PO/patient/PatientAppListPageObject.java
+++ src/test/java/mentcare/PO/patient/PatientAppListPageObject.java
@@ -1,4 +1,44 @@
 package mentcare.PO.patient;
 
-public class PatientAppListPageObject {
+import mentcare.PO.PageObject;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+
+public class PatientAppListPageObject extends PageObject {
+
+    public PatientAppListPageObject(WebDriver driver){
+        super(driver);
+    }
+
+    // ---------- Locators ----------
+    @FindBy(tagName = "h1")
+    private WebElement h1_app;
+
+    @FindBy(xpath = "//body/div[1]/div[1]/a[1]/input[1]")
+    private WebElement profile;
+
+    @FindBy(xpath = "//body/div[1]/div[2]/a[1]/input[1]")
+    private WebElement app_list;
+
+    @FindBy(xpath = "//body/div[1]/div[3]/a[1]/input[1]")
+    private WebElement logout;
+
+    // ---------- Methods ----------
+    public String Title(){
+        return h1_app.getText();
+    }
+    public ProfilePatientPageObject showProfile() {
+        this.profile.click();
+        return new ProfilePatientPageObject(driver);
+    }
+    public PatientAppListPageObject showAppList() {
+        this.app_list.click();
+        return new PatientAppListPageObject(driver);
+    }
+    public ProfilePatientPageObject logout() {
+        this.logout.click();
+        return new ProfilePatientPageObject(driver);
+    }
+
 }
