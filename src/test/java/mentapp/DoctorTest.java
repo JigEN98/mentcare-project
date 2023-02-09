fd7b1a0b42e3f870c9b91a702a645bd536fa789f
robolmatteo98 <robolmatteo98@gmail.com>
2023-02-09 19:27:37 +0100

    add scenario 4 and 5

--- src/test/java/mentapp/DoctorTest.java
+++ src/test/java/mentapp/DoctorTest.java
@@ -10,21 +10,95 @@ import static org.junit.Assert.assertEquals;
 
 public class DoctorTest extends BaseTest {
     @Test
-    public void Scenario1() {
+    // scenario 4 login del dottore
+    public void loginDoctor() {
         driver.get("http://localhost:8080/");
-        // -------------------- Login paziente --------------------
+        // -------------------- Login dottore --------------------
         LoginPageObject login_page = new LoginPageObject(driver);
-        assertEquals("Verifico che sia la pagina corretta","MentCare Login", login_page.Title());
+        assertEquals("Verifico di essere nella Home","MentCare Login", login_page.Title());
         WelcomePageDoctorPageObject welc_page = login_page.welcomedoc("lucaciano", "luca");
-        assertEquals("Verifico che sia la pagina corretta","Hello Dr. Ciano", welc_page.Title());
+        assertEquals("Verifico di aver fatto il login con il dottore ed essere nella sua Dashboard","Hello Dr. Ciano", welc_page.Title());
 
         // -------------------- Logout --------------------
         login_page = welc_page.logout();
+        assertEquals("Verifico di essere di nuovo nella Home","MentCare Login", login_page.Title());
+
+        // -------------------- Errori possibili --------------------
+        NotFoundPageObject NotFound_page = login_page.UserNotFound("mariorossi", "rossi");
+        login_page = NotFound_page.ShowLogin();
+        assertEquals("Verifico di essere ancora nella pagina login dopo l'autenticazione errata","MentCare Login", login_page.Title());
+    }
+
+    @Test
+    // scenario 5 lista pazienti dottore
+    public void listAppointments() {
+        driver.get("http://localhost:8080/");
+        // -------------------- Login dottore --------------------
+        LoginPageObject login_page = new LoginPageObject(driver);
+        WelcomePageDoctorPageObject docPage = login_page.welcomedoc("lucaciano", "luca");
+
+        assertEquals("check size list patient", 5, docPage.getSizeAppointments());
+        assertEquals("2024-10-02 09:00", docPage.getDateAppointment());
+        assertEquals("Visita glicemia", docPage.getDescriptionAppointment());
+        assertEquals("Mario Rossi", docPage.getNamePatientAppointment());
+
+        // -------------------- Logout --------------------
+        login_page = docPage.logout();
         assertEquals("Verifico che sia la pagina corretta","MentCare Login", login_page.Title());
 
         // -------------------- Errori possibili --------------------
         NotFoundPageObject NotFound_page = login_page.UserNotFound("mariorossi", "rossi");
         login_page = NotFound_page.ShowLogin();
-        assertEquals("Verifico che sia la pagina corretta","MentCare Login", login_page.Title());
+    }
+
+    @Test
+    // scenario 6 lista pazienti dottore
+    public void listPatients() {
+        driver.get("http://localhost:8080/");
+        // -------------------- Login dottore --------------------
+        LoginPageObject login_page = new LoginPageObject(driver);
+        WelcomePageDoctorPageObject docPage = login_page.welcomedoc("lucaciano", "luca");
+
+        assertEquals("check size list patient", 2, docPage.getSizePatients());
+    }
+
+    @Test
+    // scenario 7 inserimento paziente
+    public void insertPatient() {
+        driver.get("http://localhost:8080/");
+        // -------------------- Login dottore --------------------
+        LoginPageObject login_page = new LoginPageObject(driver);
+        WelcomePageDoctorPageObject docPage = login_page.welcomedoc("lucaciano", "luca");
+    }
+
+
+    @Test
+    // scenario 8 modifica paziente
+    public void modifyPatient() {
+
+    }
+
+    @Test
+    // scenario 9 eliminazione paziente
+    public void deletePatient() {
+
+    }
+
+    @Test
+    // scenario 10 inserimento appuntamento
+    public void insertAppointment() {
+
+    }
+
+    @Test
+    // scenario 11 modifica appuntamento
+    public void modifyAppointment() {
+
+    }
+
+    @Test
+    // scenario 12 eliminazione appuntamento
+    public void deleteAppointment() {
+
     }
 }
--- src/test/java/mentapp/PO/doctor/WelcomePageDoctorPageObject.java
+++ src/test/java/mentapp/PO/doctor/WelcomePageDoctorPageObject.java
@@ -4,7 +4,10 @@ import mentapp.PO.LoginPageObject;
 import mentapp.PO.PageObject;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindAll;
 import org.openqa.selenium.support.FindBy;
+
+import java.util.List;
 
 public class WelcomePageDoctorPageObject extends PageObject {
 
@@ -19,19 +22,54 @@ public class WelcomePageDoctorPageObject extends PageObject {
     @FindBy(xpath = "//body/div[1]/div[2]/a[1]/input[1]")
     private WebElement app_list;
 
-    @FindBy(tagName = "insertAppointment")
+    @FindBy(xpath = "//body/div/table/tbody/tr[1]/td[1]")
+    private WebElement dateAppointment;
+
+    @FindBy(xpath = "//body/div/table/tbody/tr[1]/td[2]")
+    private WebElement descriptionAppointment;
+
+    @FindBy(xpath = "//body/div/table/tbody/tr[1]/td[3]")
+    private WebElement namePatientAppointment;
+
+    @FindAll(@FindBy(name = "appointments"))
+    private List<WebElement> listAppointments;
+
+    @FindAll(@FindBy(name = "patients"))
+    private List<WebElement> listPatients;
+
+    @FindBy(name = "insertAppointment")
     private WebElement insertAppointment;
 
-    @FindBy(tagName = "insertPatient")
+    @FindBy(name = "insertPatient")
     private WebElement insertPatient;
 
-    @FindBy(tagName = "logout")
+    @FindBy(name = "logout")
     private WebElement logout;
 
 
     // ---------- Methods ----------
     public String Title(){
         return h1.getText();
+    }
+
+    public String getDateAppointment() {
+        return this.dateAppointment.getText();
+    }
+
+    public String getDescriptionAppointment() {
+        return this.descriptionAppointment.getText();
+    }
+
+    public String getNamePatientAppointment() {
+        return this.namePatientAppointment.getText();
+    }
+
+    public int getSizeAppointments() {
+        return this.listAppointments.size();
+    }
+
+    public int getSizePatients() {
+        return this.listPatients.size();
     }
 
     public LoginPageObject logout() {
