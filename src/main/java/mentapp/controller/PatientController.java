c7086eb6f6d773eda82936381d94d0bf5f7046d1
robolmatteo98 <robolmatteo98@gmail.com>
2023-02-13 14:09:29 +0100

    modify html

--- src/main/java/mentapp/controller/PatientController.java
+++ src/main/java/mentapp/controller/PatientController.java
@@ -69,8 +69,6 @@ public class PatientController {
                 List<Appointment> patientAppointments = appointmentRepository.findByIdPatient(pat.getID());
                 model.addAttribute("appointments", patientAppointments);
 
-                System.out.println(appointmentRepository.findByIdPatient(pat.getID()));
-
                 return "patientapplist";
             }
 
